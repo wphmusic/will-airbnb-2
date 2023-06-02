f4e3ce82016541234f981c14bbb06a4821e8039a
Will <willherrington@MacBook-Pro.attlocal.net>
2023-06-02 14:53:35 -0500

    something

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,19 +1,8 @@
 const router = require('express').Router();
 
-module.exports = router;
-
-// backend/routes/index.js
-// ...
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
-// ...
-
-// backend/routes/api/index.js
-// ...
-
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
-  
-  // ...
+
+module.exports = router;
+ 
